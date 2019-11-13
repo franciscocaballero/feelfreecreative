a7cc5f12c06dc303c06219c386de65f34e8b9056
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-11-12 21:11:40 -0500

    chagned layout

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-// let slides = document.getElementsByClassName("mySlides");
-// let dots = document.getElementsByClassName("dot");
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
 
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-// function showSlides(n) {
-//     let i;
-//     if (n > slides.length) {
-//         slideIndex = 1
-//     }
-//     if (n < 1) {
-//         slideIndex = slides.length
-//     }
+function showSlides(n) {
+    let i;
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
 
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {
-//         slideIndex = 1
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     // dots[slideIndex - 1].className += " active";
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    // dots[slideIndex - 1].className += " active";
 
-//     setTimeout(showSlides, 5000);
-// }
-// for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-// }
+    setTimeout(showSlides, 5000);
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
 
 // let myIndex = 0;
 // carousel();
